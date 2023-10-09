5da86f2889205af5779388df403986e7593b268f
Nandhu Santhosh <nandhusanthosh87@gmail.com>
2023-10-09 23:50:47 +0530

    done

--- app.js
+++ app.js
@@ -1,54 +1,42 @@
-require('dotenv').config();
+require("dotenv").config();
 
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const cookieParser = require('cookie-parser');
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
+const userRoutes = require("./routes/userRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const adminOrderRoutes = require("./routes/adminOrderRoutes");
+const utilityRoutes = require("./routes/utilityRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
 
-const userRoutes = require('./routes/userRoutes')
-const adminRoutes = require('./routes/adminRoutes')
-const adminOrderRoutes = require('./routes/adminOrderRoutes')
-const utilityRoutes = require('./routes/utilityRoutes')
-const cartRoutes = require('./routes/cartRoutes')
-const orderRoutes = require('./routes/orderRoutes')
-const paymentRoutes = require('./routes/paymentRoutes')
+const { errorHandler } = require("./Middleware/errorHandler");
 
-const {errorHandler} = require('./Middleware/errorHandler');
+app.use(express.json());
+app.use(cookieParser());
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
 
+app.use("/", userRoutes);
+app.use("/", cartRoutes);
+app.use("/order", orderRoutes);
+app.use("/admin", adminRoutes);
+app.use("/admin/orders", adminOrderRoutes);
+app.use("/payment", paymentRoutes);
+app.use("/utility", utilityRoutes);
 
+app.use(errorHandler);
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((d) => console.log("Database connection established"))
+  .catch((e) =>
+    console.log("Couldn't establish database connection: something went wrong")
+  );
 
-app.use(express.json())
-app.use(cookieParser())
-app.set('view engine', 'ejs')   
-app.use(express.static('./public'))
-
-
-app.use('/', userRoutes);
-app.use('/', cartRoutes);
-app.use('/order', orderRoutes);
-app.use('/admin', adminRoutes)
-app.use('/admin/orders', adminOrderRoutes)
-app.use('/payment', paymentRoutes)
-app.use('/utility', utilityRoutes);
-
-
-app.use(errorHandler)
-
-// TEST STARTING
-const testControllers = require('./testController');
-
-app.use('/test', testControllers)
-
-// TEST ENDING
-
-
-mongoose.connect(process.env.DB_URI)
-.then( d => console.log("Database connection established"))
-.catch( e => console.log("Couldn't establish database connection: something went wrong"))
-
-app.listen(process.env.PORT_NUMBER , '0.0.0.0', ()=>{
-    console.log("Server up on port : " , process.env.PORT_NUMBER)
-})
+app.listen(process.env.PORT || process.env.PORT_NUMBER, "0.0.0.0", () => {
+  console.log("Server up on port : ", process.env.PORT_NUMBER);
+});
--- tempCodeRunnerFile.javascript
+++
@@ -1,3 +0,0 @@
-const a = {b: 10, c: 4};
-const {b, c, d=10} = a;
-console.log(b, c, d)
